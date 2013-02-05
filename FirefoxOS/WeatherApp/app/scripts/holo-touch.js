23f3c1793e8b82320c6476dead6b5e8c2ef6791e
James Williams <james.l.williams@gmail.com>
2013-02-05 15:45:43 -0800

    Initial commit of Firefox OS weather app.

---
+++ FirefoxOS/WeatherApp/app/scripts/holo-touch.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", function(e) {
+	var inputElems = Array.prototype.slice.call(document.getElementsByTagName("button")).concat(
+		Array.prototype.slice.call(document.getElementsByTagName("select"))).concat(
+		Array.prototype.slice.call(document.getElementsByTagName("input")));
+	
+	var elemTypes = ["button", "select"];
+	var inputTypes = ["button", "submit", "reset", "radio", "checkbox"];
+	
+	for(var i = 0; i < inputElems.length; i++) {
+		if(elemTypes.indexOf(inputElems[i].tagName.toLowerCase()) != -1 || inputTypes.indexOf(inputElems[i].type.toLowerCase()) != -1) {
+			inputElems[i].addEventListener("touchstart", function(e) {
+				if(!!e.srcElement.classList) { // use classList API if available
+					e.srcElement.classList.add("active");
+				} else {
+					e.srcElement.className += " active";
+				}
+			}, false);
+			inputElems[i].addEventListener("touchend", function(e) {
+				if(!!e.srcElement.classList) { // use classList API if available
+					e.srcElement.classList.remove("active");
+				} else {
+					e.srcElement.className = e.srcElement.className.replace(/\s*active/g, "");
+				}
+			}, false);
+		}
+	}
+}, false);
