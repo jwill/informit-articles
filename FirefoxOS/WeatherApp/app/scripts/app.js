5717129c68df69705eb8393cdd21184ce01d33d3
James Williams <james.l.williams@gmail.com>
2013-02-06 10:28:23 -0800

    Switched to yepnope for resource loading. General file cleanup from droping volo. Added weather icons from http://lavana.deviantart.com/art/Flat-Weather-Icons-32021664

--- FirefoxOS/WeatherApp/app/scripts/app.js
+++ FirefoxOS/WeatherApp/app/scripts/app.js
@@ -1,30 +1,9 @@
-
-// This uses require.js to structure javascript:
-// http://requirejs.org/docs/api.html#define
-
-define(function(require) {
-    // Zepto provides nice js and DOM methods (very similar to jQuery,
-    // and a lot smaller):
-    // http://zeptojs.com/
-    var $ = require('zepto');
-
-    // Need to verify receipts? This library is included by default.
-    // https://github.com/mozilla/receiptverifier
-    require('receiptverifier');
-
-    // Want to install the app locally? This library hooks up the
-    // installation button. See <button class="install-btn"> in
-    // index.html
-    require('./install-button');
-
-    // Write your app here.
-
-
-		alert("here");
-
-
-
-
-
+yepnope({
+  load: [
+    "scripts/holo-touch.js", "scripts/vendor/zepto.min.js", "scripts/vendor/lawnchair/lawnchair-0.6.1.min.js",
+    "scripts/vendor/lawnchair/lawnchair-adapter-indexed-db-0.6.1.js"
+  ],
+  complete: function() {
+      console.log("loaded assets");
+  }
 });
-
--- FirefoxOS/WeatherApp/app/scripts/init.js
+++
@@ -1,5 +0,0 @@
-require.config({
-    baseUrl: 'scripts'
-});
-
-requirejs(['../app']);
